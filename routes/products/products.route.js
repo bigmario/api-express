7dc784263a646d6c5d5e7bdbcbd1e3f687f60a37
Mario Castro <mariocastro.pva@gmail.com>
2022-11-25 13:57:47 -0400

    services

--- routes/products/products.route.js
+++ routes/products/products.route.js
@@ -1,39 +1,20 @@
 const express = require('express')
-const { faker } = require('@faker-js/faker');
+
+const ProductsService = require('../../services/product.service')
 
 const router = express.Router();
 
-let productList = [];
-const index = 0;
-for (let index = 1; index <= 5; index++) {
-  productList.push({
-    id: index,
-    name: faker.commerce.productName(),
-    price: parseInt(faker.commerce.price()),
-    img: faker.image.imageUrl()
-  });
-}
+const prodServ = new ProductsService()
 
 router.get('/', (req, res) => {
-  const {limit, offset} = req.query;
-  const count = productList.length;
-  const pages = Math.ceil(count/limit);
-  const currentPage = Math.ceil(count%offset);
-
-  res.json({
-    data: (limit && offset) ? productList.slice((offset - 1) * limit , (limit * offset)) : productList,
-    meta: {
-      total: count,
-      showing: parseInt(limit),
-      page: currentPage,
-      pages: pages,
-    }
-  })
+  const { query } = req;
+    products = prodServ.findAll(query)
+    res.json(products);
 });
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  const product = productList.find((product) => product.id === parseInt(id))
+  const product = prodServ.findOne(id)
   if(!product) {
     res.status(404).json({
       message: 'Not found'
@@ -48,43 +29,27 @@ router.patch('/:id', (req, res) => {
   const { id } = req.params;
   const body = req.body;
 
-  const product = productList.find((item) => item.id === parseInt(id));
+  const product = prodServ.update(id, body);
 
   if (!product) {
     res.status(404).json({
       message: 'Not found'
     });
   } else {
-    for (const product of productList) {
-      if(product.id === parseInt(id)) {
-        body.name ? product.name = body.name : null;
-        body.price ? product.price = body.price: null;
-        body.img ? product.img = body.img: null;
-        break;
-      }
-    }
-
-    res.json({
-      message: 'Updated',
-      data: body
-    });
+    res.status(200).json(product);
   }
 });
 
 router.delete('/:id', (req, res) => {
   const { id } = req.params;
-  const deletedProduct = productList.find((item) => item.id === parseInt(id))
+  const deletedProduct = prodServ.delete(id)
 
   if (!deletedProduct) {
     res.status(404).json({
       message: 'Not found'
     });
   } else {
-    productList = productList.filter((item) => item.id !== parseInt(id));
-    res.json({
-      message: 'Deleted',
-      data: deletedProduct
-    });
+    res.json(deletedProduct);
   }
 });
 
---
+++ services/product.service.js
@@ -0,0 +1,85 @@
+const { faker } = require('@faker-js/faker');
+
+class ProductsService {
+  constructor() {
+    this.productList = [];
+    this.generate();
+  }
+
+  generate() {
+    for (let index = 1; index <= 5; index++) {
+      this.productList.push({
+        id: index,
+        name: faker.commerce.productName(),
+        price: parseInt(faker.commerce.price()),
+        img: faker.image.imageUrl()
+      });
+    }
+  }
+
+  create(body) {
+
+  }
+
+  findOne(id) {
+    const product = this.productList.find((product) => product.id === parseInt(id))
+    return product
+  }
+
+  findAll(queryParams = null) {
+    const {limit, offset} = queryParams;
+
+    const count = this.productList.length;
+    const pages = Math.ceil(count/limit);
+    const currentPage = Math.ceil(count%offset);
+    const products = ( (limit && offset) && ( offset>0 )) ? this.productList.slice((offset - 1) * limit , (limit * offset)) : this.productList;
+
+    return {
+      data: products,
+      meta: {
+        total: count,
+        showing: parseInt(limit) || count,
+        page: currentPage || 1,
+        pages: pages || 1,
+      }
+    }
+  }
+
+  update(id, body) {
+    const product = this.productList.find((item) => item.id === parseInt(id));
+
+    if (!product){
+      return false
+    } else {
+      for (const item of this.productList) {
+        if(product.id === parseInt(id)) {
+          body.name ? item.name = body.name : null;
+          body.price ? item.price = body.price: null;
+          body.img ? item.img = body.img: null;
+          break;
+        }
+      }
+      return {
+        message: 'Updated',
+        data: body
+      }
+    }
+  }
+
+  delete(id) {
+    const deletedProduct = this.productList.find((item) => item.id === parseInt(id))
+
+    if (!deletedProduct) {
+      return false
+    } else {
+      this.productList = this.productList.filter((item) => item.id !== parseInt(id));
+      return {
+        message: 'Deleted',
+        data: deletedProduct
+      };
+    }
+
+  }
+}
+
+module.exports = ProductsService;
