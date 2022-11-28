7658164f2a3c577aac83b355c8d113a5ae28abfe
Mario Castro <mariocastro.pva@gmail.com>
2022-11-28 11:27:41 -0400

    added new routes & docker compose file

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
-apiRouter = require('./api/api.route')
-productsRouter = require('./products/products.route')
+const apiRouter = require('./api/api.route');
+const productsRouter = require('./products/products.router');
+const categoriesRouter = require('./categories/categories.router');
+const usersRouter = require('./users/users.router');
+const orderRouter = require('./orders/orders.router');
+const customersRouter = require('./customers/customers.router');
 
 
 function routerApi(app) {
   app.use('/api', apiRouter);
   apiRouter.use('/products', productsRouter);
+  apiRouter.use('/categories', categoriesRouter);
+  apiRouter.use('/users', usersRouter);
+  apiRouter.use('/orders', orderRouter);
+  apiRouter.use('/customers', customersRouter);
 }
 
 module.exports = routerApi;
